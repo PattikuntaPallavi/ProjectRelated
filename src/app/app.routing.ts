07854b9d8ddb347cc5b88ee66ea3ecd8e67a7f19
Pallavi Pattikuntapalli <papatt32@in.ibm.com>
2019-04-16 10:28:52 +0530

    UI

---
+++ src/app/app.routing.ts
@@ -0,0 +1,22 @@
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AuthGuard } from './_guards/auth.guard';
+import { NotfoundComponent } from "./notfound/notfound.component";
+import { PostsComponent } from "./posts/posts.component";
+import { LandingPageComponent } from './landing-page/landing-page.component';
+
+const appRoutes: Routes = [
+    { path: '', component: LandingPageComponent },
+    { path: 'home', component: HomeComponent},
+    { path: 'selfposts', component: PostsComponent},
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+
+    // otherwise redirect to home
+    { path: '**', component: NotfoundComponent }
+];
+
+export const routing = RouterModule.forRoot(appRoutes);
